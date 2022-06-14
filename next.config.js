ea632c4734d33aadb6ecbfeebf020c1ae8b2c75c
HossamElharmeil <hossam.elharmil@gmail.com>
2022-06-14 17:27:27 +0200

    header component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['www.redditinc.com', 'links.papareact.com', 'logoeps.com'],
+  },
 }
